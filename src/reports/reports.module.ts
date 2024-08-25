e8ebecee3846a0c1a4d512a8d288c8a671f45815
Miguel Martínez <migueljomartinez@gmail.com>
2024-08-25 18:41:24 -0500

    Adding User and Report entities

--- src/reports/reports.module.ts
+++ src/reports/reports.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { ReportsController } from './reports.controller'
 import { ReportsService } from './reports.service'
+import { Report } from './report.entity'
 
 @Module({
   controllers: [ReportsController],
   providers: [ReportsService],
+  imports: [TypeOrmModule.forFeature([Report])],
 })
 export class ReportsModule {}
--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common'
+import { TypeOrmModule } from '@nestjs/typeorm'
 
 import { UsersService } from './users.service'
 import { UsersController } from './users.controller'
+import { User } from './user.entity'
 
 @Module({
   providers: [UsersService],
   controllers: [UsersController],
+  // Create repository for User entity
+  imports: [TypeOrmModule.forFeature([User])],
 })
 export class UsersModule {}
