43c63bc9a2a1311c6de74185dc6b96b95237fcd5
Miguel Martínez <migueljomartinez@gmail.com>
2024-09-03 19:26:09 -0500

    Excluding password from the response usng an Interceptor

--- src/interceptors/serialize.interceptor.ts
+++ src/interceptors/serialize.interceptor.ts
@@ -8,18 +8,24 @@ import { Observable } from 'rxjs'
 import { map } from 'rxjs/operators'
 import { plainToClass } from 'class-transformer'
 
+import { UserDto } from '../users/dtos/user.dto'
+
 export class SerializeInterceptor implements NestInterceptor {
   intercept(
     context: ExecutionContext,
     next: CallHandler<any>,
   ): Observable<any> | Promise<Observable<any>> {
     // Run something before a request is handled by the request handler
-    console.log('I am running before the handler', context)
+    console.log('I am running before the handler')
 
     return next.handle().pipe(
       map((data: any) => {
         // Run something before the response is sent out
-        console.log('I am running before response is sent out', data)
+        console.log('I am running before response is sent out')
+
+        return plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        })
       }),
     )
   }
