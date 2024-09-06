4c4039aad86dcf768cb2d5819257c754cbc02b29
Miguel Martínez <migueljomartinez@gmail.com>
2024-09-05 19:18:08 -0500

    WIP: AuthService signup function

--- src/users/auth.service.ts
+++ src/users/auth.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common'
+import { BadRequestException, Injectable } from '@nestjs/common'
 import { UsersService } from './users.service'
 
 @Injectable()
@@ -7,11 +7,25 @@ export class AuthService {
 
   async signup(email: string, password: string) {
     console.log('Signup', email, password)
+    // See if email is in use
+    const users = await this.userService.findByEmail(email)
+    if (users.length > 0) {
+      throw new BadRequestException('Email already in use')
+    }
+
+    // Hash the user's password
+
+    // Create a new user and save it
+
+    // Respond with the user
+  }
+  async signin(email: string, password: string) {
     /**
-     * 1. Check if a user with the email already exists, if it does, throw a ConflictException
-     * 2. Encrypt the password
-     * 3. Store the new user record in the database
-     * 4. Send back a cookie that contains the user's id
+     * 1. Check if a user with the provided email exists, if it doesn't, throw a NotFoundException
+     * 2. Decrypt the password
+     * 3. Compare the provided password with the decrypted password
+     *  - If they don't match, throw a BadRequestException
+     *  - If they do match, send back a cookie that contains the user's id
      */
   }
 }
