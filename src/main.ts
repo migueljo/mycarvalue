007f1d0c176f7e86eef879911936aa93583c1ed4
Miguel Martínez <migueljomartinez@gmail.com>
2024-09-09 19:39:50 -0500

    Setting up cookie-session

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,18 @@
 import { NestFactory } from '@nestjs/core'
 import { ValidationPipe } from '@nestjs/common'
 
+const cookieSession = require('cookie-session')
+
 import { AppModule } from './app.module'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
+  app.use(
+    cookieSession({
+      // TODO: Improvie this
+      keys: ['asdasdasd'],
+    }),
+  )
   app.useGlobalPipes(
     new ValidationPipe({
       // Strip away any properties that are not in the DTO
