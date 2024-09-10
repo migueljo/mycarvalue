bdfacf029917fc8baf0718136b5429c63b8ad5f7
Miguel Martínez <migueljomartinez@gmail.com>
2024-09-10 18:54:48 -0500

    WIP: Creating @CurrentUser custom decorator 2

--- src/users/decorators/current-user.decorator.ts
+++ src/users/decorators/current-user.decorator.ts
@@ -2,7 +2,9 @@ import { createParamDecorator } from '@nestjs/common'
 import { ExecutionContextHost } from '@nestjs/core/helpers/execution-context-host'
 
 export const CurrentUser = createParamDecorator(
-  (data: any, context: ExecutionContextHost) => {
+  (data: never, context: ExecutionContextHost) => {
+    const request = context.switchToHttp().getRequest()
+    console.log('@CurrentUser()', request.session.userId)
     return 'hi there'
   },
 )
